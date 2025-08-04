aa21133b805930a785d4052a9b0696a5204d4c3a
K N Maswanganye <Khomananimokoena@gmail.com>
2025-08-05 00:00:45 +0200

    Add files via upload

---
+++ Memebership.java
@@ -0,0 +1,13 @@
+
+package memebership;
+import  Membership.m.Membershipp;
+public class Memebership {
+
+    
+    public static void main(String[] args) {
+       Membershipp mp = new Membershipp();
+       
+       mp.setVisible(true);
+    }
+    
+}
